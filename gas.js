cd5a7ce3b0a27658864d56b20866240b7881b805
mirage2000b <macross.kuo@gmail.com>
2022-12-22 13:58:40 +0800

    v1.0

---
+++ gas.js
@@ -0,0 +1,17 @@
+
+/*=====================================
+  20221222
+=====================================*/
+function test() {
+    //取得試算表
+    let ss = SpreadsheetApp.getActiveSpreadsheet();
+    //取得工作表
+    let ws = ss.getSheetByName('工作表7');
+    //取得範圍
+    let range = ws.getRange(1,1);
+
+    //寫資料
+    range.setValue('單位');
+
+    console.log(ws);
+}
